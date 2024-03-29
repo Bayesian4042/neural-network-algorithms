09189e1765b52de94d155fc2a808929cef87924c
Abhilasha <singhabhilasha4042@gmail.com>
2024-03-29 20:40:56 +0530

    adds bert code

--- transformers/bert/bert.py
+++ transformers/bert/bert.py
@@ -16,3 +16,40 @@ class BertEmbeddings(nn.Module):
         segment_embeddings = self.segment_embeddings(segment_ids)
         position_embeddings = self.position_embeddings(self.pos_input)
         return token_embeddings + segment_embeddings + position_embeddings
+
+
+class Bert(nn.Module):
+    def __init__(self, vocab_size, embed_size, n_segments, max_sequence_length, n_layers, attn_heads, dropout=0.1):
+        super().__init__()
+        self.embeddings = BertEmbeddings(vocab_size, embed_size, n_segments, max_sequence_length, dropout)
+        self.encoder = nn.TransformerEncoderLayer(embed_size, attn_heads, embed_size * 4)
+        self.encoder_block = nn.TransformerEncoder(self.encoder_layer, n_layers)
+    
+    def forward(self, x, segment_ids):
+        x = self.embeddings(x, segment_ids)
+        return self.encoder_block(x)
+
+def main():
+    VOCAB_SIZE = 30000
+    N_SEGMENTS = 3
+    MAX_LEN = 512
+
+    # Architecture
+    EMBEDDING_DIM = 768
+    N_LAYERS = 12
+    ATTN_HEADS = 12
+    DROPOUT = 0.1
+
+    # Training
+    BATCH_SIZE = 32
+
+    sample_seq = torch.randint(high=VOCAB_SIZE, size=[MAX_LEN,])
+    sample_seg = torch.randint(high=N_SEGMENTS, size=[MAX_LEN,])
+
+    embeddings = BertEmbeddings(VOCAB_SIZE, EMBEDDING_DIM, N_SEGMENTS, MAX_LEN, DROPOUT)
+    embeddings_tensor = embeddings(sample_seq, sample_seg)
+    print(embeddings_tensor.shape)
+
+    bert = Bert(VOCAB_SIZE, EMBEDDING_DIM, N_SEGMENTS, MAX_LEN, N_LAYERS, ATTN_HEADS, DROPOUT, ATTN_HEADS)
+    bert_out = bert(sample_seq, sample_seg)
+    print(bert_out.shape)
